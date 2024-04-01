978b466a1e3dbb806d0af48eedbf43d653c996b2
unknown <fanin457@gmail.com>
2024-04-01 22:08:46 +0200

    Dodanie pliku opisującego co dostajemy na wejściu i co musimy wyprodukować na wyjściu

---
+++ wejscie-wyjscie.R
@@ -0,0 +1,17 @@
+Na wejściu dostajemy:
+  - odległość pionu sondażowego od lewego brzegu
+  - głębokość w pionie sondażowym
+  - które z pionów sondażowych są pionami pomiarowymi
+  - współczynniki alfa i beta młynku pomiarowego (dwa sety dla różnych zasięgów liczby obrotów)
+  - głębokość pomiarów w pionie pomiarowym, liczba obrotów młynka i czas pomiaru.
+
+
+Na wyjściu wydajemy:
+  - Przepływ całkowity
+  - szkic przekroju, ale to nie obowiązkowe
+
+Założenia ogólne:
+  - na starcie zakladamy, że dostajemy idealne dane bez braków i normalnie pomierzone
+  - w pierwszym kroku robimy jądro, które oblicza te idealne dane
+  - w drugim kroku obsługujemy przypadki niepoprawnych danych
+  - w trzecim kroku piszemy komunikacje z użytkownikiem, ilekolwiek czasu będziemy na to mieli
