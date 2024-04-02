c30fe3f3ddfe6212c379d62e84e2b2cd337500cd
unknown <fanin457@gmail.com>
2024-04-02 23:58:19 +0200

    (Na razie skrutowo) opisana instrukcja, aka pierwsza komunikacja z użytkownikiem.

---
+++ instrukcja.R
@@ -0,0 +1,6 @@
+Instrukcja dla użytkownika:
+  pobierz dwa pliki .csv, które dla ciebie wygenerowaliśmy
+weź je w teren, uzupełnij dane i zapisz.
+Pola bez wartości mają zostać puste
+Podaj ścieżkę do pliku z pomiarami głębokości w przekroju:
+Podaj ścieżkę do pliku z pomiarami prędkości w pionach:
