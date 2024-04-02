8b80104122371249e38db458fc30d2f07b47c318
unknown <fanin457@gmail.com>
2024-04-02 14:03:44 +0200

    Poprawki w czytelności i efektywnosci oblicznia średnich głębokości i odległości pomiędzy pionami

--- obliczenia_przyklad.R
+++ obliczenia_przyklad.R
@@ -10,36 +10,60 @@ df_predkosci
 
 df_glebokosci
 
-gleb_srednia = round(c(((df_glebokosci$glebokosc[[1]] + df_glebokosci$glebokosc[[2]])/2), 
-                 ((df_glebokosci$glebokosc[[2]] + df_glebokosci$glebokosc[[3]])/2),
-                 ((df_glebokosci$glebokosc[[3]] + df_glebokosci$glebokosc[[4]])/2),
-                 ((df_glebokosci$glebokosc[[4]] + df_glebokosci$glebokosc[[5]])/2),
-                 ((df_glebokosci$glebokosc[[5]] + df_glebokosci$glebokosc[[6]])/2),
-                 ((df_glebokosci$glebokosc[[6]] + df_glebokosci$glebokosc[[7]])/2),
-                 ((df_glebokosci$glebokosc[[7]] + df_glebokosci$glebokosc[[8]])/2),
-                 ((df_glebokosci$glebokosc[[8]] + df_glebokosci$glebokosc[[9]])/2),
-                 ((df_glebokosci$glebokosc[[9]] + df_glebokosci$glebokosc[[10]])/2),
-                 ((df_glebokosci$glebokosc[[10]] + df_glebokosci$glebokosc[[11]])/2),
-                 ((df_glebokosci$glebokosc[[11]] + df_glebokosci$glebokosc[[12]])/2),
-                 ((df_glebokosci$glebokosc[[12]] + df_glebokosci$glebokosc[[13]])/2),
-                 ((df_glebokosci$glebokosc[[13]] + df_glebokosci$glebokosc[[14]])/2),
-                 ((df_glebokosci$glebokosc[[14]] + df_glebokosci$glebokosc[[15]])/2)), 2)
+df_glebokosci$odl_miedzy_pionami = c(rep(0,15))
+#oblicza odległość między pionami sondażowymi
+for (i in c(2:15)){
+  df_glebokosci$odl_miedzy_pionami[[i]] = df_glebokosci$odl_od_brzegu_lew[[i]] - df_glebokosci$odl_od_brzegu_lew[[i-1]]
+}
 
-gleb_srednia
+df_glebokosci$gl_srednia = c(rep(0,15))
+#oblicza średnią głębokość między pionami sondażowymi
+for (i in c(2:15)){
+  df_glebokosci$gl_srednia[[i]] = (df_glebokosci$glebokosc[[i]] + df_glebokosci$glebokosc[[i-1]])/2
+}
 
-odl_miedzy_punkt = c((df_glebokosci$odl_od_brzegu_lew[[2]] - df_glebokosci$odl_od_brzegu_lew[[1]]),
-                     (df_glebokosci$odl_od_brzegu_lew[[3]] - df_glebokosci$odl_od_brzegu_lew[[2]]),
-                     (df_glebokosci$odl_od_brzegu_lew[[4]] - df_glebokosci$odl_od_brzegu_lew[[3]]),
-                     (df_glebokosci$odl_od_brzegu_lew[[5]] - df_glebokosci$odl_od_brzegu_lew[[4]]),
-                     (df_glebokosci$odl_od_brzegu_lew[[6]] - df_glebokosci$odl_od_brzegu_lew[[5]]),
-                     (df_glebokosci$odl_od_brzegu_lew[[7]] - df_glebokosci$odl_od_brzegu_lew[[6]]),
-                     (df_glebokosci$odl_od_brzegu_lew[[8]] - df_glebokosci$odl_od_brzegu_lew[[7]]),
-                     (df_glebokosci$odl_od_brzegu_lew[[9]] - df_glebokosci$odl_od_brzegu_lew[[8]]),
-                     (df_glebokosci$odl_od_brzegu_lew[[10]] - df_glebokosci$odl_od_brzegu_lew[[9]]),
-                     (df_glebokosci$odl_od_brzegu_lew[[11]] - df_glebokosci$odl_od_brzegu_lew[[10]]),
-                     (df_glebokosci$odl_od_brzegu_lew[[12]] - df_glebokosci$odl_od_brzegu_lew[[11]]),
-                     (df_glebokosci$odl_od_brzegu_lew[[13]] - df_glebokosci$odl_od_brzegu_lew[[12]]),
-                     (df_glebokosci$odl_od_brzegu_lew[[14]] - df_glebokosci$odl_od_brzegu_lew[[13]]),
-                     (df_glebokosci$odl_od_brzegu_lew[[15]] - df_glebokosci$odl_od_brzegu_lew[[14]]))
+#gleb_srednia = round(c(((df_glebokosci$glebokosc[[1]] + df_glebokosci$glebokosc[[2]])/2), 
+#                ((df_glebokosci$glebokosc[[2]] + df_glebokosci$glebokosc[[3]])/2),
+#                 ((df_glebokosci$glebokosc[[3]] + df_glebokosci$glebokosc[[4]])/2),
+#                 ((df_glebokosci$glebokosc[[4]] + df_glebokosci$glebokosc[[5]])/2),
+#                 ((df_glebokosci$glebokosc[[5]] + df_glebokosci$glebokosc[[6]])/2),
+#                 ((df_glebokosci$glebokosc[[6]] + df_glebokosci$glebokosc[[7]])/2),
+#                 ((df_glebokosci$glebokosc[[7]] + df_glebokosci$glebokosc[[8]])/2),
+#                 ((df_glebokosci$glebokosc[[8]] + df_glebokosci$glebokosc[[9]])/2),
+#                 ((df_glebokosci$glebokosc[[9]] + df_glebokosci$glebokosc[[10]])/2),
+#                 ((df_glebokosci$glebokosc[[10]] + df_glebokosci$glebokosc[[11]])/2),
+#                 ((df_glebokosci$glebokosc[[11]] + df_glebokosci$glebokosc[[12]])/2),
+#                 ((df_glebokosci$glebokosc[[12]] + df_glebokosci$glebokosc[[13]])/2),
+#                 ((df_glebokosci$glebokosc[[13]] + df_glebokosci$glebokosc[[14]])/2),
+#                 ((df_glebokosci$glebokosc[[14]] + df_glebokosci$glebokosc[[15]])/2)), 2)
 
-odl_miedzy_punkt
+#gleb_srednia
+
+#odl_miedzy_punkt = c((df_glebokosci$odl_od_brzegu_lew[[2]] - df_glebokosci$odl_od_brzegu_lew[[1]]),
+#                     (df_glebokosci$odl_od_brzegu_lew[[3]] - df_glebokosci$odl_od_brzegu_lew[[2]]),
+#                     (df_glebokosci$odl_od_brzegu_lew[[4]] - df_glebokosci$odl_od_brzegu_lew[[3]]),
+#                     (df_glebokosci$odl_od_brzegu_lew[[5]] - df_glebokosci$odl_od_brzegu_lew[[4]]),
+#                     (df_glebokosci$odl_od_brzegu_lew[[6]] - df_glebokosci$odl_od_brzegu_lew[[5]]),
+#                     (df_glebokosci$odl_od_brzegu_lew[[7]] - df_glebokosci$odl_od_brzegu_lew[[6]]),
+#                     (df_glebokosci$odl_od_brzegu_lew[[8]] - df_glebokosci$odl_od_brzegu_lew[[7]]),
+#                     (df_glebokosci$odl_od_brzegu_lew[[9]] - df_glebokosci$odl_od_brzegu_lew[[8]]),
+#                     (df_glebokosci$odl_od_brzegu_lew[[10]] - df_glebokosci$odl_od_brzegu_lew[[9]]),
+#                     (df_glebokosci$odl_od_brzegu_lew[[11]] - df_glebokosci$odl_od_brzegu_lew[[10]]),
+#                     (df_glebokosci$odl_od_brzegu_lew[[12]] - df_glebokosci$odl_od_brzegu_lew[[11]]),
+#                     (df_glebokosci$odl_od_brzegu_lew[[13]] - df_glebokosci$odl_od_brzegu_lew[[12]]),
+#                     (df_glebokosci$odl_od_brzegu_lew[[14]] - df_glebokosci$odl_od_brzegu_lew[[13]]),
+#                     (df_glebokosci$odl_od_brzegu_lew[[15]] - df_glebokosci$odl_od_brzegu_lew[[14]]))
+
+#odl_miedzy_punkt
+
+df_glebokosci$powierzchnia = c(rep(0,15))
+#oblicza odległość między pionami sondażowymi
+for (i in c(1:15)){
+  df_glebokosci$powierzchnia[[i]] = df_glebokosci$odl_miedzy_pionami[[i]] * df_glebokosci$gl_srednia[[i]]
+}
+copy = df_glebokosci
+for (i in c(1:15)) {
+  if (copy$nr_pionu[[i]] == '0'){
+    copy$nr_pionu[[i]] = NA
+  }
+}
