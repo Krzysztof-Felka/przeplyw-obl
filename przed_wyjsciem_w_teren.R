19ac5f2f968633ac8fecebedf011f064f94598df
unknown <fanin457@gmail.com>
2024-04-03 04:23:25 +0200

    Poprawki, to już wersja do przesłania

--- przed_wyjsciem_w_teren.R
+++ przed_wyjsciem_w_teren.R
@@ -3,11 +3,16 @@
 # W "sondowanie_glebokosci.csv" 1 kolumna to odległość od brzegu, 2 kolumna to numer pionów, 3 kolumna to głębokość
 
 do_roboty = function(){
+  wd = getwd()
+  cat("Ten skrypt stworzy folder 'Arkusze do uzupełnienia' w ", wd)
+  cat("\nW tym folderze zostaną umieszczone dwa pliki .csv, które należy wypełnić danymi, otrzymanymi z pomiarów w terenie")
   sondowanie_glebokosci = data.frame(odległość_od_brzegu_lewego = c(0),
                                      nr_pionu = c(0),
                                      głębokość = c(0))
+  dir.create("Arkusze do uzupelnienia")
   write.csv(sondowanie_glebokosci,
-            file = "dane do uzupełnienia z terenu/01sondowanie_glebokosci.csv")
+            file = "Arkusze do uzupelnienia/01sondowanie_glebokosci.csv",
+            row.names = F)
   # Zostaje stworzony plik z 3 kolumnami i jednym wierszem z wartością zero, należy zamienić ten wiersz pierwszym pomiarem, i następnie dodać kolejne wiersze aż do ostatniego pomiaru.
   pomiar_predkosci = data.frame(Pion_pomiarowy = c(0),
                                 odl_od_brzegu_lewego = c(0),
@@ -20,7 +25,8 @@ do_roboty = function(){
                                 "l obr 0,2 h" = c(0),
                                 l_obr_dno = c(0))
   write.csv(pomiar_predkosci,
-            file = "dane do uzupełnienia z terenu/01pomiar_predkosci.csv")
+            file = "Arkusze do uzupelnienia/01pomiar_predkosci.csv",
+            row.names = F)
   # Zostaje stworzony plik z 10 kolumnami i jeden wiersz z wartościami 0, które należy zamienić na pierwszy pomiar, następnie kolejny wiersz na drugi pomiar, aż do ostatniego pomiaru.
 }
-do_roboty
+do_roboty()
