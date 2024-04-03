56ffb21c9ae2c4e740783bc99e390e6ba794e121
unknown <fanin457@gmail.com>
2024-04-03 02:06:22 +0200

    dodano liczbę obrotów na sekundę, sprawdzanie, czy wsystkie pomiary w zakresie pomiarowym narzędzia

--- obliczenia_poprawione.R
+++ obliczenia_poprawione.R
@@ -12,10 +12,46 @@ piony_pomiar = nrow(predkosci)
 
 #obliczenie średniej prędkości przepływu wody w pionach pomiarowych ----
 
-#obliczenie liczby obrotów na sekundę ----
-for (i in 1:piony_pomiar) {
-  predkosci$n_pow = predkosci$l_obr_pow / predkosci$czas_pomiaru
-  
+#obliczenie liczby obrotów na sekundę n ----
+#czas pomiaru col 4
+#l_obr_pow col 5
+#n_obr_pow col 11
+#v_pow col 17
+for (i in 0:5) {
+  predkosci[,11+i] = predkosci[,5+i] / predkosci[,4]
 }
+
+#obliczenie prędkości przepływu w pionie pomiarowym ----
+# deklaracja współczynników, USUNĄĆ JAK BĘDZIE ODCZYT OD UŻYTKOWNIKA
+alfa1 = 0.014
+alfa2 = 0.004
+beta1 = 0.247
+beta2 = 0.261
+granica_min1 = 0 
+granica_min2 = 7
+granica_max1 = 71 
+granica_max2 = 100
+
+#sprawdzanie, czy liczba zmierzonych obrotów nie wychodzi poza zakres maksymalny młynka i ewentualne
+#poinformowanie użytkownika, że obliczenia nie będą dokładne, bo pomiar wychodzi poza zakres przyrządu
+licznik = 0
+for (j in 0:5) {
+  
+  for (i in 1:piony_pomiar) {
+    
+    if (!is.na(predkosci[i,5+j]) && predkosci[i,5+j]>= max(c(granica_max1, granica_max2))) {
+      licznik = licznik + 1
+    }
+  
+  }
+}
+if (licznik !=0 ) {
+  
+  cat("Twoje dane zawierają wartości z poza zakresu pomiarowego młynka.\nLiczba pomiarów z poza zakresu:\n",licznik, 
+      "\nWykonane obliczenia przepływu nie będą dokładne")
+}
+
+#sprawdzenie, jakie granice są większe, jakie wsp alfa i beta muszą być użyte do wartości 
+#i obliczanie prędkości przepływu w punkcie
 
 
